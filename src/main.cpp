ed544ab38096ba57a57c40e9baaa59d8a9a36fd2
Student528052 <528052@student.saxion.nl>
2024-01-20 15:49:02 +0100

    added settings to doxyconfig

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 //template
 int main(void){
     VM::VM_temp vm;
-    std::vector<i32> prog{10, 7, 0x40000001,0x40000000};
+    std::vector<i32> prog{10, 7, 0x40000010,0x40000000};
     vm.loadProg(prog);
     vm.run();
     return 0;
