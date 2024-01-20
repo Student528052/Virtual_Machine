b5c31579cb532e5ae5a0191543db8ebb06be249e
Tashomudev <stilianbatinkov03@gmail.com>
2024-01-20 19:42:23 +0200

    Created rnp.h

---
+++ RNP_Lang/RNP.h
@@ -0,0 +1,28 @@
+//
+// Created by Lenovo on 1/20/2024.
+//
+
+#ifndef VIRTUAL_MACHINE_RNP_H
+#define VIRTUAL_MACHINE_RNP_H
+#include <iostream>
+#include <vector>
+
+typedef uint8_t byte;
+typedef std::vector<std::string> strings;
+
+enum State : byte {
+    START,
+    READCHAR,
+    READBLOCK,
+    END
+};
+
+class Lexer {
+    bool my_isspace(char c);
+    bool isspecial(char c);
+    bool isgroup(char c);
+    char end_char, beg_char;
+public:
+    strings rnp(std::string s);
+};
+#endif //VIRTUAL_MACHINE_RNP_H
